34cd3417d262d81896403d5da6d3058e6c54cbca
Tostes, Rogerio <rogertotes@yahoo.com.br>
2020-05-27 00:03:36 -0300

    adding project to search repositories and their data

---
+++ IC2020/Models/ObjStructure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IC2020.Models
+{
+    public class Collabs
+    {
+        public string login { get; set; }
+        public int id { get; set; }
+    }
+
+    public class ObjStructure
+    {
+        public Item generalInfo { get; set; }
+        public List<Collabs> collaborators { get; set; }
+
+        public ObjStructure()
+        {
+            collaborators = new List<Collabs>();
+        }
+
+    }
+}
---
+++ IC2020/Models/Repos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IC2020.Models
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string full_name { get; set; }
+        public string description { get; set; }
+        public string contributors_url { get; set; }
+        public DateTime updated_at { get; set; }
+        public string language { get; set; }
+    }
+
+    public class Repos
+    {
+        public List<Item> items { get; set; }
+    }
+}
